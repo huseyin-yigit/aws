0eae34726408c84c78e8336df3278ee47b8168fa
huseyin-yigit <63133251+huseyin-yigit@users.noreply.github.com>
2020-09-17 11:41:21 -0400

    added millisecond converter

--- projects/002-milliseconds-converter/milliseconds-converter-app.py
+++ projects/002-milliseconds-converter/milliseconds-converter-app.py
@@ -18,19 +18,19 @@ def convert(milliseconds):
 
 @app.route('/', methods=['GET'])
 def main_get():
-    return render_template('index.html', developer_name='Tyler - Mentor', not_valid=False)
+    return render_template('index.html', developer_name='Huseyin Yigit', not_valid=False)
 
 
 @app.route('/', methods=['POST'])
 def main_post():
     alpha = request.form['number']
     if not alpha.isdecimal():
-        return render_template('index.html', developer_name='Tyler - Mentor', not_valid=True)
+        return render_template('index.html', developer_name='Huseyin Yigit', not_valid=True)
 
     number = int(alpha)
     if not (0 < number):
-        return render_template('index.html', developer_name='Tyler - Mentor', not_valid=True)
-    return render_template('result.html', milliseconds=number, result=convert(number), developer_name='Tyler - Mentor')
+        return render_template('index.html', developer_name='Huseyin Yigit', not_valid=True)
+    return render_template('result.html', milliseconds=number, result=convert(number), developer_name='Huseyin Yigit')
 
 
 if __name__ == '__main__':
